7e3918d16874b2ab762386d20cada8c251280b60
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-04-05 11:19:27 +0200

    Bump HPDCache

--- core/cache_subsystem/cva6_hpdcache_subsystem.sv
+++ core/cache_subsystem/cva6_hpdcache_subsystem.sv
@@ -231,6 +231,7 @@ module cva6_hpdcache_subsystem
     userCfg.wbEn =
         (CVA6Cfg.DCacheType == config_pkg::HPDCACHE_WB) ||
         (CVA6Cfg.DCacheType == config_pkg::HPDCACHE_WT_WB);
+    userCfg.lowLatency = 1;
     return userCfg;
   endfunction
 
