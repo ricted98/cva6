1e37ad09445aaf92a5e14c820cebb1d5de95b325
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2026-03-19 23:47:59 +0100

    cva6_hpdcache_subsystem: add dedicated package for configuration

--- core/cache_subsystem/cva6_hpdcache_subsystem.sv
+++ core/cache_subsystem/cva6_hpdcache_subsystem.sv
@@ -129,14 +129,6 @@ module cva6_hpdcache_subsystem
 );
   //  }}}
 
-  function int unsigned __minu(int unsigned x, int unsigned y);
-    return x < y ? x : y;
-  endfunction
-
-  function int unsigned __maxu(int unsigned x, int unsigned y);
-    return y < x ? x : y;
-  endfunction
-
   //  I$ instantiation
   //  {{{
   logic icache_miss_valid, icache_miss_ready;
@@ -178,72 +170,8 @@ module cva6_hpdcache_subsystem
   //  {{{
   `include "hpdcache_typedef.svh"
 
-  //    0: Page-Table Walk (PTW)
-  //    1: Load unit
-  //    2: Accelerator load
-  //    3: Store/AMO
-  //    .
-  //    .
-  //    .
-  //    NumPorts: CMO
-  //    NumPorts + 1: Hardware Memory Prefetcher (hwpf)
-  localparam int HPDCACHE_NREQUESTERS = NumPorts + 2;
-
-  function automatic hpdcache_pkg::hpdcache_user_cfg_t hpdcacheSetConfig();
-    hpdcache_pkg::hpdcache_user_cfg_t userCfg;
-    userCfg.nRequesters = HPDCACHE_NREQUESTERS;
-    userCfg.paWidth = CVA6Cfg.PLEN;
-    userCfg.wordWidth = CVA6Cfg.XLEN;
-    userCfg.sets = CVA6Cfg.DCACHE_NUM_WORDS;
-    userCfg.ways = CVA6Cfg.DCACHE_SET_ASSOC;
-    userCfg.clWords = CVA6Cfg.DCACHE_LINE_WIDTH / CVA6Cfg.XLEN;
-    userCfg.reqWords = 1;
-    userCfg.reqTransIdWidth = CVA6Cfg.DcacheIdWidth;
-    userCfg.reqSrcIdWidth = 3;  // Up to 8 requesters
-    userCfg.victimSel = hpdcache_pkg::HPDCACHE_VICTIM_RANDOM;
-    userCfg.dataWaysPerRamWord = __minu(CVA6Cfg.DCACHE_SET_ASSOC, 128 / CVA6Cfg.XLEN);
-    userCfg.dataSetsPerRam = CVA6Cfg.DCACHE_NUM_WORDS;
-    userCfg.dataRamByteEnable = 1'b1;
-    userCfg.accessWords = __maxu(CVA6Cfg.AxiDataWidth / CVA6Cfg.XLEN, userCfg.reqWords);
-    userCfg.mshrSets = CVA6Cfg.NrLoadBufEntries < 16 ? 1 : CVA6Cfg.NrLoadBufEntries / 2;
-    userCfg.mshrWays = CVA6Cfg.NrLoadBufEntries < 16 ? CVA6Cfg.NrLoadBufEntries : 2;
-    userCfg.mshrWaysPerRamWord = CVA6Cfg.NrLoadBufEntries < 16 ? CVA6Cfg.NrLoadBufEntries : 2;
-    userCfg.mshrSetsPerRam = CVA6Cfg.NrLoadBufEntries < 16 ? 1 : CVA6Cfg.NrLoadBufEntries / 2;
-    userCfg.mshrRamByteEnable = 1'b1;
-    userCfg.mshrUseRegbank = (CVA6Cfg.NrLoadBufEntries < 16);
-    /*FIXME we should add additional CVA6 config parameters (cbufEntries)*/
-    userCfg.cbufEntries = 4;
-    userCfg.refillCoreRspFeedthrough = 1'b1;
-    userCfg.refillFifoDepth = 2 * (CVA6Cfg.DCACHE_LINE_WIDTH / CVA6Cfg.AxiDataWidth);
-    userCfg.wbufDirEntries = CVA6Cfg.WtDcacheWbufDepth;
-    userCfg.wbufDataEntries = CVA6Cfg.WtDcacheWbufDepth;
-    userCfg.wbufWords = 1;
-    userCfg.wbufTimecntWidth = 3;
-    userCfg.rtabEntries = 4;
-    /*FIXME we should add additional CVA6 config parameters (flushEntries)*/
-    userCfg.flushEntries = CVA6Cfg.WtDcacheWbufDepth;
-    /*FIXME we should add additional CVA6 config parameters (flushFifoDepth)*/
-    userCfg.flushFifoDepth = CVA6Cfg.WtDcacheWbufDepth;
-    userCfg.memAddrWidth = CVA6Cfg.AxiAddrWidth;
-    userCfg.memIdWidth = CVA6Cfg.MEM_TID_WIDTH;
-    userCfg.memDataWidth = CVA6Cfg.AxiDataWidth;
-    userCfg.wtEn =
-        (CVA6Cfg.DCacheType == config_pkg::HPDCACHE_WT) ||
-        (CVA6Cfg.DCacheType == config_pkg::HPDCACHE_WT_WB);
-    userCfg.wbEn =
-        (CVA6Cfg.DCacheType == config_pkg::HPDCACHE_WB) ||
-        (CVA6Cfg.DCacheType == config_pkg::HPDCACHE_WT_WB);
-    userCfg.lowLatency = 1'b1;
-    userCfg.eccEn = 1'b0;
-    userCfg.eccScrubberEn = 1'b0;
-    userCfg.externalSram = 1'b0;
-    return userCfg;
-  endfunction
-
-  localparam hpdcache_pkg::hpdcache_user_cfg_t HPDcacheUserCfg = hpdcacheSetConfig();
-  localparam hpdcache_pkg::hpdcache_cfg_t HPDcacheCfg = hpdcache_pkg::hpdcacheBuildConfig(
-      HPDcacheUserCfg
-  );
+  localparam hpdcache_pkg::hpdcache_cfg_t HPDcacheCfg =
+      cva6_hpdcache_subsystem_pkg::hpdcacheBuildCfg(CVA6Cfg, NumPorts);
 
   `HPDCACHE_TYPEDEF_MEM_ATTR_T(hpdcache_mem_addr_t, hpdcache_mem_id_t, hpdcache_mem_data_t,
                                hpdcache_mem_be_t, HPDcacheCfg);
